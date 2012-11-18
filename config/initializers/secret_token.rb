eb592c99c792053416afbf3d6e19c1aff7d5138d
tomoya ueda <to.ueda82@gmail.com>
2012-11-18 13:55:52 +0900

    initial commit. project usagamiso-re start!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UsagamisoRe::Application.config.secret_token = '6b4ff9ff8827ef5b1e69f7f3ff2a7c161637a614c58dd4ffcfafb350207aab70216d869ebd93e2af234925e8411a0c59e292d79ba81e1ff723018cf23dbab00d'
