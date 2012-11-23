4f2f05531177f59c6d45c728867a10bb95d20757
satomicchy <satomi_t_sora@yahoo.co.jp>
2012-11-24 00:17:44 +0900

    implement controller#to_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,11 @@ class ApplicationController < ActionController::Base
     puts session[:user_id]
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def to_login
+    redirect_to root_path if @current_user.nil?
+  end
+
   helper_method :current_user
+
 end
